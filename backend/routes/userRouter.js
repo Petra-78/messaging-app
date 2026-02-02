50ab6f1fec8b18eabba816c3b016ca1c4a07f760
Petra-78 <partos.petra.0305@gmail.com>
2026-02-02 18:50:42 +0100

    Add user updating route

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getUsers, getUser } from "../controllers/userController.js";
+import {
+  getUsers,
+  getUser,
+  updateUser,
+} from "../controllers/userController.js";
 import { verifyClient } from "../auth/authMiddleware.js";
 const router = Router();
 
 router.get("/users", verifyClient, getUsers);
 router.get("/user", verifyClient, getUser);
+router.put("/user", verifyClient, updateUser);
 
 export { router as userRouter };
