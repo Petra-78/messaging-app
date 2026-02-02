1a39bedc5ab7cb28ec419f97ad44374c68796e6f
Petra-78 <partos.petra.0305@gmail.com>
2026-02-02 15:58:59 +0100

    Create basic server

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+import { authRouter } from "./routes/authRouter.js";
+import { verifyClient } from "./auth/authMiddleware.js";
+
+
+
+app.use("/", authRouter);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Blog API running" });
+});
+
+app.get("/hi", verifyClient, (req,res)=>{
+  res.json({message:"hi!"})
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
