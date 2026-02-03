9f3f97f50768b5af06f0731fa490c5a144c6003b
Petra-78 <partos.petra.0305@gmail.com>
2026-02-03 13:51:23 +0100

    Remove cors config

--- backend/app.js
+++ backend/app.js
@@ -4,16 +4,8 @@ import "dotenv/config";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }),
-);
+app.use(cors());
 
-app.options("*", cors());
 app.use(express.json());
 
 import { authRouter } from "./routes/authRouter.js";
