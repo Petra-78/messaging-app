588c9634be12c5b7219a260b8ec57757bfdb9514
Petra-78 <partos.petra.0305@gmail.com>
2026-02-03 18:49:10 +0100

    Add user redirect if not logged in

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,34 @@
 import { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Chat from "../components/Chat";
+import { useAuth } from "../context/authContext";
+import { Navigate } from "react-router-dom";
 
 export default function Home() {
   const [selectedUser, setSelectedUser] = useState(null);
+  const { user, authLoading } = useAuth();
+
+  debugger;
+
+  if (authLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
 
   return (
     <>
-      <div>
-        <Sidebar
-          selectedUser={selectedUser}
-          setSelectedUser={setSelectedUser}
-        />
-        <Chat selectedUser={selectedUser} />
-      </div>
+      {user && (
+        <div>
+          <Sidebar
+            selectedUser={selectedUser}
+            setSelectedUser={setSelectedUser}
+          />
+          <Chat selectedUser={selectedUser} />
+        </div>
+      )}
     </>
   );
 }
