d53cacd292a389b9c820d74b0d473477686a6b95
Petra-78 <partos.petra.0305@gmail.com>
2026-02-03 12:54:59 +0100

    Add login component

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { useAuth } from "../../context/authContext";
+import styles from "./Login.module.css";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+      const res = await fetch(
+        "https://blog-api-production-323f.up.railway.app/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        },
+      );
+
+      if (!res.ok) {
+        throw new Error(res.message());
+      }
+
+      const data = await res.json();
+      login(data.token, data.user);
+
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.card}>
+        <h2 className={styles.title}>Login</h2>
+
+        {error && <p className={styles.error}>{error}</p>}
+
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input
+            className={styles.input}
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <input
+            className={styles.input}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <button className={styles.button} type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
