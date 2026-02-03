861bda89105f63918b40a8d4db7b852d76df9fd4
Petra-78 <partos.petra.0305@gmail.com>
2026-02-03 18:00:35 +0100

    Add chat component + states

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,18 @@
+import { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
+import Chat from "../components/Chat";
 
 export default function Home() {
+  const [selectedUser, setSelectedUser] = useState(null);
+
   return (
     <>
       <div>
-        <Sidebar />
+        <Sidebar
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
+        <Chat selectedUser={selectedUser} />
       </div>
     </>
   );
