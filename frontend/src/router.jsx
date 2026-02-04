7312b5e00a6eb4029060a2df4335bc28a7815121
Petra-78 <partos.petra.0305@gmail.com>
2026-02-04 14:32:24 +0100

    Uncomment error route

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,5 +1,5 @@
 import App from "./App";
-// import Error from "./components/Error/Error";
+import Error from "./components/Error";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
@@ -8,7 +8,7 @@ const routes = [
   {
     path: "/",
     element: <App />,
-    // errorElement: <Error />,
+    errorElement: <Error />,
     children: [
       { index: true, element: <Home /> },
       { path: "/login", element: <Login /> },
