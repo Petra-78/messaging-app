c5a64a56629d556d6407f776f12502f03c0d0d08
Petra-78 <partos.petra.0305@gmail.com>
2026-02-04 14:33:38 +0100

    Add footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+export default function Footer() {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faGithub} />
+      <p>Petra-78</p>
+    </div>
+  );
+}
