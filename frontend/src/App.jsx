f565f43dcbfe1300cdedefe8fa203c746cb442bf
Petra-78 <partos.petra.0305@gmail.com>
2026-02-06 17:45:52 +0100

    Add tostify container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,21 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { useState } from "react";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const [selectedUser, setSelectedUser] = useState(null);
   const [showUserInfo, setShowUserInfo] = useState(false);
   return (
     <>
-      <div className="flex flex-col h-screen">
+      <div className="flex flex-col h-screen overflow-hidden">
         <Navbar
           setSelectedUser={setSelectedUser}
           setShowUserInfo={setShowUserInfo}
           showUserInfo={showUserInfo}
         />
         <Outlet
+          className="flex-1 min-h-0 overflow-hidden"
           context={{
             selectedUser,
             setSelectedUser,
@@ -24,6 +26,17 @@ function App() {
           }}
         />
         <Footer />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </div>
     </>
   );
