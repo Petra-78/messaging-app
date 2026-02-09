c1ca75994a5852a1b2503b6882ddbb9a0183a048
Petra-78 <partos.petra.0305@gmail.com>
2026-02-09 20:49:04 +0100

    Add global chat header

--- frontend/src/components/chat/ChatHeader.jsx
+++ frontend/src/components/chat/ChatHeader.jsx
@@ -1,12 +1,27 @@
 export default function ChatHeader({ user }) {
   return (
     <div className="flex justify-center items-center gap-4 border-b border-gray-300 pb-2 mb-4">
-      <img
-        className="h-8 w-8 rounded-full object-cover"
-        src={user.avatarUrl || "/placeholder.png"}
-        alt=""
-      />
-      <h2 className="text-lg font-semibold text-gray-800">{user.username}</h2>
+      {user === "global" ? (
+        <>
+          <img
+            className="h-8 w-8 rounded-full object-cover"
+            src={"/global-chat.png"}
+            alt=""
+          />
+          <h2 className="text-lg font-semibold text-gray-800">Global Chat</h2>
+        </>
+      ) : (
+        <>
+          <img
+            className="h-8 w-8 rounded-full object-cover"
+            src={user.avatarUrl || "/placeholder.png"}
+            alt=""
+          />
+          <h2 className="text-lg font-semibold text-gray-800">
+            {user.username}
+          </h2>
+        </>
+      )}
     </div>
   );
 }
