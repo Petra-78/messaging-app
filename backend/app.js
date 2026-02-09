9b2caa0ad2d96bcbc17463843d4c31851dd119d0
Petra-78 <partos.petra.0305@gmail.com>
2026-02-09 21:16:27 +0100

    Add vercel domain to cors config

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://world-talk.netlify.app/", "http://localhost:5173"],
+    origin: [
+      "https://world-talk.netlify.app/",
+      "https://world-talk-app.vercel.app",
+      "http://localhost:5173",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
